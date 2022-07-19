18bda211d56d944b42aac17e9f32259ad5c6789e
Alexander garcia <105325837+alexgarpul@users.noreply.github.com>
2022-07-18 21:12:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let name = 'alexander', 
+lastname = 'garcia', 
+yearOfBirth = 2001, 
+cityOfBirth = 'medellin'
+
+console.log(
+    `Hola, mi nombre es ${name} ${lastname}, vivo en ${cityOfBirth} y tengo ${2022 - yearOfBirth} años.`
+)
